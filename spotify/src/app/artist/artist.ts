b7e3d3723a9ac5af2c6028840c92c2532d67420c
Sanni <117445321+fsannicolo@users.noreply.github.com>
2026-01-21 11:05:38 +0000

    feat:dati artista e album

--- spotify/src/app/artist/artist.ts
+++ spotify/src/app/artist/artist.ts
@@ -1,6 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SpotifyService } from '../spotify-service';
+import { IArtist } from '../interfaces/i-artist';
 
 @Component({
   selector: 'app-artist',
@@ -12,10 +13,18 @@ export class Artist implements OnInit {
   private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private spotifyService: SpotifyService = inject(SpotifyService);
 
+  artist: WritableSignal<IArtist | null> = signal(null);
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(urlParams => {
       let id: string = urlParams['id'];
-      console.log(urlParams);
+      this.spotifyService.getArtist(id).subscribe((dati: IArtist) => {
+        this.artist.set(dati);
+      })
     })
+  }
+
+  get genres(): string {
+    return this.artist()!.genres.join(', ');
   }
 }
